12ffc4448e21c595a443df768029f3b1c368d531
TrainHardLiveEasy <kolleradam2005@gmail.com>
2018-03-29 20:57:18 -0500

    making changes

--- caesar.py
+++ caesar.py
@@ -1,18 +1,39 @@
-from helpers import alphabet_position, rotate_character
+def alphabet_position(character):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    lower = character.lower()
+    return alphabet.index(lower)
 
-def encrypt(text, rot):
-    cipher = ''
-    for value in text:
-        if value.isalpha():             
-            cipher += rotate_character(value,rot)
+def rotate_string_13(text):
+
+    rotated = ''
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    for char in text:
+        rotated_idx = (alphabet_position(char) + 13) % 26
+        if char.isupper():
+            rotated = rotated + alphabet[rotated_idx].upper()
         else:
-            cipher += value
-    return cipher
-def main():
-    text = raw_input("Type a message: ")  
-    rot = raw_input("Rotate by: ")
-    rot = int(rot)                
-    print (encrypt(text,rot))
+            rotated = rotated + alphabet[rotated_idx]
 
-if __name__ == '__main__':
-    main()
+    return rotated
+
+def rotate_character(char, rot):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    rotated_idx = (alphabet_position(char) + rot) % 26
+
+    if char.isupper():
+        return alphabet[rotated_idx].upper()
+    else:
+        return alphabet[rotated_idx]
+
+def rotate_string(text, rot):
+
+    rotated = ''
+
+    for char in text:
+        if (char.isalpha()):
+            rotated = rotated + rotate_character(char, rot)
+        else:
+            rotated = rotated + char
+
+return rotated
